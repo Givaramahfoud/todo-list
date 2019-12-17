084f73b47c7dc62b8d9fa3bc6b3cd3ae1483ca0c
givara <givoo25@gmail.com>
2019-12-17 17:22:20 +0100

    Updates

---
+++ precache-manifest.83c790fc33c586b374a7e1a38ab92a28.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57996144b0436a79ad7f8031355d20c0",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "1b2c11d9189f063f43b9",
+    "url": "/todo-list/static/css/main.9804957a.chunk.css"
+  },
+  {
+    "revision": "041f16a11a4d37928eda",
+    "url": "/todo-list/static/js/2.7d0d6359.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todo-list/static/js/2.7d0d6359.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1b2c11d9189f063f43b9",
+    "url": "/todo-list/static/js/main.caab72a9.chunk.js"
+  },
+  {
+    "revision": "2138ed1788c343766253",
+    "url": "/todo-list/static/js/runtime-main.1ff8b4e7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo-list/static/media/logo.5d5d9eef.svg"
+  }
+]);
